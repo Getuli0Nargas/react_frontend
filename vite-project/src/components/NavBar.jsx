a672ec878cd7cc30a1093cf28917b57f2822c469
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-12 14:15:31 -0300

    react22

---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+export default function NavBar(){
+ return(
+    <>
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>contato</li>
+                <li>cadastrar</li>
+            </ul>
+        </nav>
+    
+    
+    
+    </>
+
+
+
+ )
+
+
+}
